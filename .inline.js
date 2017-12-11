fafec6cd1260838a2a1323e0fa437625a665a7fa
Jimmy Lin <jmlntw@gmail.com>
2017-12-11 20:14:26 +0800

    Use ES6 object shorthand notation

--- .inline.js
+++ .inline.js
@@ -2,25 +2,25 @@ module.exports = {
   shortcuts: [
     {
       name: 'pkg',
-      expand: function (file, ...args) {
+      expand (file, ...args) {
         return `../package.json|parse:${args.join()}`
       }
     },
     {
       name: 'css',
-      expand: function (file, css, indent = '0') {
+      expand (file, css, indent = '0') {
         return `css/${css}.css|cssmin|indent:${indent}|trim`
       }
     },
     {
       name: 'data',
-      expand: function (file, data, indent = '0') {
+      expand (file, data, indent = '0') {
         return `data/${data}.json|stringify:120|indent:${indent}|trim`
       }
     },
     {
       name: 'template',
-      expand: function (file, template, indent = '0') {
+      expand (file, template, indent = '0') {
         return `templates/${template}.html|indent:${indent}|trim`
       }
     }
@@ -29,21 +29,21 @@ module.exports = {
   transforms: [
     {
       name: 'indent',
-      transform: function (file, content, size = '2', indent = ' ') {
+      transform (file, content, size = '2', indent = ' ') {
         const indentString = require('indent-string')
         return indentString(content, parseInt(size, 10), { indent: indent })
       }
     },
     {
       name: 'cssmin',
-      transform: function (file, content) {
+      transform (file, content) {
         const CleanCSS = require('clean-css')
         return new CleanCSS({ keepBreaks: true }).minify(content).styles
       }
     },
     {
       name: 'stringify',
-      transform: function (file, content, maxLength = '80') {
+      transform (file, content, maxLength = '80') {
         const stringifyObject = require('stringify-object')
         return stringifyObject(JSON.parse(content), {
           indent: '  ',
