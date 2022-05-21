56837116ff77ffbb5b2170ad20d1733ca03c4219
Jimmy Lin <jmlntw@gmail.com>
2022-05-21 14:02:36 +0800

    Release v0.2.9

--- dist/google-search-region.meta.js
+++ dist/google-search-region.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Google Search Region
 // @namespace       jmln.tw
-// @version         0.2.8
+// @version         0.2.9
 // @description     A user script that lets you quickly switch Google search to different region.
 // @author          Jimmy Lin
 // @license         MIT
